3d1c62cb6596420ab4906999c8727166d87b1c1b
khangover27@gmail.com <khangover27@gmail.com>
2025-10-10 13:53:01 +0700

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'KukuIt | Worklog',
   description: 'KukuIt Worklog System',
+  icons: {
+    icon: '/favicon.ico'
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
