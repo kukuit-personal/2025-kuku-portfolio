1f197512364350fe225d6f39c429bbf626bdba3f
khangover27@gmail.com <khangover27@gmail.com>
2025-10-30 19:20:33 +0700

    create /about

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <input id="nav-toggle" type="checkbox" className="peer sr-only" />
 
           {/* Sidebar (off-canvas mobile, fixed width desktop) */}
-          <NavbarLeft />
+          {/* <NavbarLeft /> */}
 
           {/* Main area */}
           <div className="flex-1 flex flex-col">
